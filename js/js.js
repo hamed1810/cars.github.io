1773988e1634b9c1ea56319930939df01c3d7ca7
hamed shoorgeshti <89836825+hamed1810@users.noreply.github.com>
2024-01-11 19:35:54 +0330

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true, // نقطه های راست
+    navigationTooltips: ['صفحه نخست', 'خدمات ما', 'محصولات ما', 'تماس باما', 'درباره ما'], // متن کنار نقطه ها
+    showActiveTooltip: true, // متن اکتیو کنار هر دایره را نشان میدهد
+    loopBottom: true,
+    loopTop: true
+});
